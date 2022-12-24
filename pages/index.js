a2a3dc855085f2eafcf3e5a7ff5ccc49a4a2f77d
Pabllot07 <pabllot07@outlook.com>
2022-12-24 14:04:06 -0300

    bg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import { Sidebar } from '../components/Sidebar/Sidebar'
 import { Video } from '../components/Video/Video'
