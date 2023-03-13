8e95c29dbcc69397dce22d018aeb3a5d0dc15ffe
Pabllot07 <pabllot07@outlook.com>
2023-03-12 21:20:39 -0300

    new data

--- src/components/Card/styles.jsx
+++ src/components/Card/styles.jsx
@@ -1,64 +1,94 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 470px;
-height: 108px;
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 8px;
-cursor: pointer;
+  width: 470px;
+  height: 108px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  cursor: pointer;
 
-&:hover {
+  &:hover {
     background-color: #a2a0a047;
-}
+  }
 
-@media (max-width: 1024px) { width: 320px; height: 94px}
-@media (max-width: 768px) { width: 100%; height: 18%}
-@media (max-width: 620px) {width: 90%;  height: 18%}
-@media (max-width: 350px) { width: 90%; height: 18%}
-
-`
+  @media (max-width: 1024px) {
+    width: 320px;
+    height: 94px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 18%;
+  }
+  @media (max-width: 620px) {
+    width: 90%;
+    height: 18%;
+  }
+  @media (max-width: 350px) {
+    width: 90%;
+    height: 18%;
+  }
+`;
 export const Right = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    width: 175px;
-    height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 175px;
+  height: 100px;
 
-    @media (max-width: 768px) {height: 85%; width: 50%}
-    @media (max-width: 620px) {height: 85%; width: 50%}
-    @media (max-width: 350px) {width: 50%; height: 85%}
-`
+  @media (max-width: 768px) {
+    height: 85%;
+    width: 50%;
+  }
+  @media (max-width: 620px) {
+    height: 85%;
+    width: 50%;
+  }
+  @media (max-width: 350px) {
+    width: 50%;
+    height: 85%;
+  }
+`;
 
 export const Left = styled.img`
-    width: 150px;
-    height: 100px;
-    border-radius: 4px;
+  width: 150px;
+  height: 90%;
+  border-radius: 4px;
+  object-fit: cover;
 
-    @media (max-width: 1024px) {width: 125px}
-    @media (max-width: 768px) {height: 85%; width: 33%}
-    @media (max-width: 620px) { height: 85%; width: 45%}
-    @media (max-width: 350px) {width: 40%; height: 85%}
- `
+  @media (max-width: 1024px) {
+    width: 125px;
+  }
+  @media (max-width: 768px) {
+    height: 85%;
+    width: 33%;
+  }
+  @media (max-width: 620px) {
+    height: 85%;
+    width: 45%;
+  }
+  @media (max-width: 350px) {
+    width: 40%;
+    height: 85%;
+  }
+`;
 
- export const Title = styled.div`
-    color: ${props => props.theme.colors.secondary};
-    font-size: 15px;
-    font-weight: 600;
-    margin: 0;   
-
-`
- export const Author = styled.p`
-    color: ${props => props.theme.colors.secondary};
-    font-size: 12px;
-    font-weight: 300;
-    margin: 0;
-`
- export const Views = styled.p`
-    color: ${props => props.theme.colors.secondary};
-    font-size: 11px;
-    font-weight: 300;
-    margin: 0;
-
-`
+export const Title = styled.div`
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 15px;
+  font-weight: 600;
+  margin: 0;
+`;
+export const Author = styled.p`
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 12px;
+  font-weight: 300;
+  margin: 0;
+`;
+export const Views = styled.p`
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 11px;
+  font-weight: 300;
+  margin: 0;
+`;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,39 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { Card } from '../Card/Card'
-import { Container, Sidebarr, Filter, Button, LargeButton, Icon } from './styles';
-import { BsSunFill} from 'react-icons/bs'
-import { IoMdMoon } from 'react-icons/io'
+import React, { useEffect, useState } from "react";
+import { Card } from "../Card/Card";
+import {
+  Container,
+  Sidebarr,
+  Filter,
+  Button,
+  LargeButton,
+  Icon,
+} from "./styles";
+import { BsSunFill } from "react-icons/bs";
+import { IoMdMoon } from "react-icons/io";
 
-export const Sidebar = ({setChosenVideo, toggleTheme, theme, data }) => {
-  const [filter, setFilter] = useState('kids');
+export const Sidebar = ({ setChosenVideo, toggleTheme, theme, data }) => {
+  const [filter, setFilter] = useState("kids");
   const [filteredVids, setFilteredVids] = useState([]);
 
-
-  // vai filtrar todos os videos do db e retornar somente os que tem o mesmo genero, 
-  useEffect(()=>{
-    setFilteredVids([])
-      for(let i = 0; i < data.length; i++){
-        if(data[i].genre === filter) setFilteredVids(prev => [...prev, data[i]])  
+  // vai filtrar todos os videos do db e retornar somente os que tem o mesmo genero,
+  useEffect(() => {
+    setFilteredVids([]);
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].genre === filter)
+        setFilteredVids((prev) => [...prev, data[i]]);
     }
-  },[filter])
-
+  }, [filter]);
 
   return (
-    <Container >
-        <Sidebarr>
-          <Filter> 
-            <Button onClick={()=>setFilter('kids')}>Movies</Button>
-            <LargeButton onClick={()=>setFilter('office')}>The Office</LargeButton>
-            <Button onClick={()=>setFilter('cats')}>Cats</Button>
-            <Button onClick={()=>setFilter('dogs')}>Dogs</Button>
-            {/*<Icon onClick={toggleTheme}>{theme.title === 'light' ? <IoMdMoon/> : <BsSunFill/>}</Icon>*/}
-          </Filter>
+    <Container>
+      <Sidebarr>
+        <Filter>
+          <Button onClick={() => setFilter("kids")}>Movies</Button>
+          <LargeButton onClick={() => setFilter("office")}>
+            The Office
+          </LargeButton>
+          {/*<Icon onClick={toggleTheme}>{theme.title === 'light' ? <IoMdMoon/> : <BsSunFill/>}</Icon>*/}
+        </Filter>
 
-       {filteredVids.slice(0, 5).map(item=><Card theme={theme} setChosenVideo={setChosenVideo} url={item.url}
-        key={item.id} name={item.name} author={item.author} views={item.views} img={item.img} alt={item.alt}/>       
-       )}
- 
+        {filteredVids.slice(0, 5).map((item) => (
+          <Card
+            theme={theme}
+            setChosenVideo={setChosenVideo}
+            url={item.url}
+            key={item.id}
+            name={item.name}
+            author={item.author}
+            views={item.views}
+            img={item.img}
+            alt={item.alt}
+          />
+        ))}
       </Sidebarr>
     </Container>
-  )
-}
+  );
+};
